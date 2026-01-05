aa5de16ba1ee09ca0ad427df3d0443282fcb0426
SizhenChen <chens656@newschool.edu>
2026-01-04 21:00:47 -0800

    up

---
+++ scriptCHN.js
@@ -0,0 +1,13 @@
+//获取图片
+const container = document.getElementById("imgContainer");
+
+const imageCount = 11; // 中文图片数量
+const imagePath = "imagesCHN/"; // 图片所在目录
+
+for (let i = 1; i <= imageCount; i++) {
+    const img = document.createElement("img");
+    img.src = imagePath + i + ".jpg";
+    img.alt = "image " + i;
+
+    container.appendChild(img);
+}
